f6a55fe4bef8336360ad5ba3ce969da5a1b8e778
Alexander Reinthal <reinthal@net.chalmers.se>
2017-04-24 11:01:38 +0200

    added plot

--- Stability.R
+++ Stability.R
@@ -7,11 +7,11 @@ library(lasso2)
 library(glmnet)
 library(lqa)
 library(bestglm)
+library(MASS)
+library(hdi)
 
-# sample(1:10,3,replace = FALSE)
 # Cook dome data
-
-ns<-100
+ns<-200
 var1 <- 1
 covar1 <- 0.9
 sigma_matrix <- as.matrix( cbind(c(var1,covar1) ,c(covar1,var1) ))
@@ -44,58 +44,79 @@ yDat <- y
 xDat <- xx
 
 # create the resampled data indeces
-nr_bootstraps <- 30
+nr_truevars <- length(bs[bs != 0])
 nr_datapoints <- dim(df)[1]
+nr_bootstraps <- 30
+nr_iterations <- 20
 fracs <- seq(1,0.1,length=nr_bootstraps)
 
-se1_errMATRIX <- matrix(0,length(fracs),2)
-min_errMATRIX<- matrix(0,length(fracs),2)
 
-i <- 1
-nr_iterations <- 50
-frac <- fracs[i]
-nr_samples <- round( nr_datapoints * frac )
+oracle_errMATRIX <- array(0, dim=c(nr_bootstraps,nr_iterations,2))
+pval_matrix <- array(0,dim=c(nr_truevars,nr_bootstraps,nr_iterations,2))
+
+
+min_errMATRIX[,,1] <- fracs
+pval_matrix[,,,1] <- fracs
+
+# Unit Test the loop
+iBootstrap <- 1
+iIteration <- 1
+frac <- 0.5
+nr_samples <- round( nr_datapoints * fracs[iBootstrap] )
 bootstrap <- sample( 1:nr_datapoints , size = nr_samples, replace = FALSE)
 cv.gg<-cv.glmnet(x= xDat[bootstrap,], y=yDat[bootstrap])
+ll <- lasso.proj(x=xDat[bootstrap,],y=yDat[bootstrap])
+
+beta_hats <- coef(cv.gg,s="lambda.min")[-1]
+min_err <- t(beta_hats - bs) %*% (beta_hats - bs)
+pval_matrix[,iBootstrap,iIteration,2] <- ll$pval[bs != 0]
 
 
-se1_matrix <- coef(cv.gg,s="lambda.1se")[-1]
-min_matrix <- coef(cv.gg,s="lambda.min")[-1]
-se1_err <- t(se1_matrix - bs) %*% (se1_matrix - bs)
-min_err <- t(min_matrix - bs) %*% (min_matrix - bs)
-
-
-
+# Loop the loop, get errors - get rewarded
 for (iIteration in (1:nr_iterations)) {
+  
   print( paste("iteration: ", iIteration) )
-  for (i in (1:length(fracs))){
-    frac <- fracs[i]
+  
+  for (iBootstrap in (1:nr_bootstraps)){
+  
     
-    # sample with replacement
-    nr_samples <- round( nr_datapoints * frac )
+    # sample without replacement
+    nr_samples <- round( nr_datapoints * fracs[iBootstrap] )
     bootstrap <- sample( 1:nr_datapoints , size = nr_samples, replace = FALSE)
-    cv.gg<-cv.glmnet(x= xDat[bootstrap,], y=yDat[bootstrap])
+    #cv.gg <- cv.glmnet(x= xDat[bootstrap,], y = yDat[bootstrap])
+    ll <- lasso.proj(x = xDat[bootstrap,],y = yDat[bootstrap])
     
+   
+    #beta_hats <- coef(cv.gg,s="lambda.min")[-1]
+    #min_err <- t(beta_hats - bs) %*% (beta_hats - bs)
     
-    se1_matrix <- coef(cv.gg,s="lambda.1se")[-1]
-    min_matrix <- coef(cv.gg,s="lambda.min")[-1]
-    se1_err <- t(se1_matrix - bs) %*% (se1_matrix - bs)
-    min_err <- t(min_matrix - bs) %*% (min_matrix - bs)
-    
-    se1_errMATRIX[i,1] <- frac
-    se1_errMATRIX[i,2] <- se1_errMATRIX[i,2]+se1_err
-    
-    min_errMATRIX[i,1] <- frac
-    min_errMATRIX[i,2] <- min_errMATRIX[i,2]+min_err
+    #oracle_errMATRIX[iBootstrap,iIteration,2] <- min_err
+    pval_matrix[,iBootstrap,iIteration,2] <- ll$pval[bs != 0]
     
     
   }
 }
-se1_errMATRIX[,2] <- se1_errMATRIX[,2] / nr_iterations
-min_errMATRIX[,2] <- min_errMATRIX[,2] / nr_iterations
+# Calculate the mean and std dev. from oracle error
+M1 <- t(oracle_errMATRIX[,,2])
+mus <- colMeans(M1)
+sigmas <- apply(M1, 2, sd)
 
-plot(se1_errMATRIX[,1], sqrt(se1_errMATRIX[,2])
+# calculate the points +1 sd
+p1sd <- mus+sigmas
+
+
+# calculate the points -1 sd
+m1sd <- mus-sigmas
+
+
+
+# Make new plot function
+plot(fracs,mus,
+     type="b"
      ,xlab="Fraction of total data",
-     ylab="Squared Error in Beta",
-     main="Stability of LASSO")
+     ylab="Oracle Squared Error",
+     main="Stability of LASSO",lty=3)
+lines(fracs,p1sd,col="green",lty=1)
+lines(fracs,m1sd,col="green",lty=1)
+
 
